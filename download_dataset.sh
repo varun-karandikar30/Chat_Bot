8f24fc06ae778fa27573cf82d6b0362870d0ca7d
Shivam Dwivedi <12083769+panditshivam@users.noreply.github.com>
2019-03-28 17:49:42 +0530

    Create download_dataset.sh

---
+++ download_dataset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://www.repository.cam.ac.uk/bitstream/handle/1810/280608/MULTIWOZ2.zip?sequence=3&isAllowed=y
+# wget https://figureqadataset.azureedge.net #https://datasets.maluuba.com/Frames/dl
+# wget www.cs.cmu.edu/~ark/QA-data/data/Question_Answer_Dataset_v1.2.tar.gz
+# Microsoft Research WikiQA Corpus  from https://www.microsoft.com/en-us/download/confirmation.aspx?id=52419
+# Other available at https://webscope.sandbox.yahoo.com/catalog.php?datatype=l&guccounter=1
+# https://trec.nist.gov/data/qa.html
